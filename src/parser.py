6cf748ece9b72d7a6d4ff0a456060a4d67a996d6
valeriyaduran <valeriya.duran@gmail.com>
2023-11-19 20:07:41 +0300

    test data removed quotes changed to double for consistency

--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,15 @@
-import yaml
+from yaml import safe_load
 
 
 class YamlParser:
-    _build_file_path = 'builds/builds.yaml'
-    _tasks_file_path = 'builds/tasks.yaml'
+    _build_file_path = "builds/builds.yaml"
+    _tasks_file_path = "builds/tasks.yaml"
 
     @classmethod
     def parse_build_and_tasks_yaml_files(cls) -> dict:
-        with open(cls._build_file_path, 'r') as builds_file, open(cls._tasks_file_path, 'r') as tasks_file:
-            builds = yaml.safe_load(builds_file)
-            tasks = yaml.safe_load(tasks_file)
+        with open(cls._build_file_path, "r") as builds_file, open(cls._tasks_file_path, "r") as tasks_file:
+            builds = safe_load(builds_file)
+            tasks = safe_load(tasks_file)
             merged_data = {**builds, **tasks}
         return merged_data
 
