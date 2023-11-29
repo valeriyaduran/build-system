162483231f174ddc4c5bb05753edf681b14905d6
valeriyaduran <valeriya.duran@gmail.com>
2023-11-29 12:14:14 +0300

    check api_key moved to Dependencies

---
+++ src/dependencies.py
@@ -0,0 +1,11 @@
+import os
+
+from fastapi import Header, HTTPException
+from typing_extensions import Annotated
+
+
+async def verify_api_key(api_key: Annotated[str, Header()]):
+    if not api_key:
+        raise HTTPException(status_code=400, detail="No API-Key provided")
+    elif api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=400, detail="API-Key is invalid")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import json
-import os
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Depends
 
 from handlers.exception_handler import BuildNotFoundException, build_not_found_exception_handler
+from src.dependencies import verify_api_key
 from src.models import Build
 from src.config import builds_and_tasks
 from src.parser import YamlParser
@@ -19,21 +19,18 @@ async def lifespan(app: FastAPI):
     builds_and_tasks.clear()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(dependencies=[Depends(verify_api_key)], lifespan=lifespan)
 app.add_exception_handler(BuildNotFoundException, build_not_found_exception_handler)
 
 
 @app.post("/get_tasks")
 async def get_tasks(build_data: Build, request: Request) -> Response:
-    if request.headers.get('Authorization') == os.getenv('API_KEY'):
-        build_name = build_data.build
-        if not build_name:
-            return Response(status_code=400, content="Message: Empty build name")
-        try:
-            task_manager = TaskManager(build_name=build_name, extracted_data=builds_and_tasks["data"]())
-            sorted_tasks = task_manager.get_sorted_tasks()
-        except IndexError:
-            raise BuildNotFoundException(build_name)
-        return Response(status_code=200, content=json.dumps(sorted_tasks))
-    else:
-        return Response(status_code=401, content="Message: Not authorized")
+    build_name = build_data.build
+    if not build_name:
+        return Response(status_code=400, content="Message: Empty build name")
+    try:
+        task_manager = TaskManager(build_name=build_name, extracted_data=builds_and_tasks["data"]())
+        sorted_tasks = task_manager.get_sorted_tasks()
+    except IndexError:
+        raise BuildNotFoundException(build_name)
+    return Response(status_code=200, content=json.dumps(sorted_tasks))
